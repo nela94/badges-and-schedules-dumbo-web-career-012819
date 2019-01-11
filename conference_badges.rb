7cf7a315e9b12ac54bb02a929d39d6a65ee7c01b
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-11 15:07:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,5 @@ def badge_maker(name)
   badges = "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badges.each {|i|}
 end
