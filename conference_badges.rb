fab5831f5116122d9f73c3c7381c76326690a199
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-11 15:51:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(attendees)
   welcome =[]
   i = 1
-  room_assignments = attendees.each do |name,i|
+  room_assignments = attendees.each do |name|
     welcome.push("Hello, #{name}! You'll be assigned to room #{i}!")
     i +=1
   end
