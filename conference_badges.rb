a1dc19381fdcd891b3f794eaad95663e2d83dd6a
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-11 15:54:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,5 +20,8 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badges|
     puts badges
-  end 
+  end
+  assigned_rooms(attendees).each do |room_assignments|
+    puts room_assignments
+  end
 end
